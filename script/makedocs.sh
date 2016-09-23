94228236364d7f3b79f036a3b1faeb8f9acd2fdd
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2016-09-23 17:56:52 -0400

    initial commit

---
+++ script/makedocs.sh
@@ -0,0 +1,5 @@
+# show python version just to be sure we're using the right one.
+python --version
+
+# simulating build process
+echo "v1" >"$1/README"
---
+++ script/pushdocs.sh
@@ -0,0 +1,16 @@
+DOCS_REPO_NAME="travis-test-target"
+DOCS_REPO_URL="git@github.com:sbstp/travis-test-target.git"
+
+openssl aes-256-cbc -K $encrypted_7d232113fa70_key -iv $encrypted_7d232113fa70_iv -in key.enc -out key -d
+ssh-add key
+rm key
+
+git clone "$DOCS_REPO_URL" "$DOCS_REPO_NAME"
+bash script/makedocs.sh "$DOCS_REPO_NAME"
+
+cd "$DOCS_REPO_NAME"
+git add --all
+# Check if anythhing changed, and if it's the case, push to origin/master.
+if git commit --author="Automated Update <>" -m 'update docs' ; then
+    git push origin master
+fi
